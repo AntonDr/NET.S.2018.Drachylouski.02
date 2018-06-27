333685c8b3c1f2423f7767327e6a93e30c7da01a
Anton Drachylouski <31773079+AntonDr@users.noreply.github.com>
2018-06-27 17:35:52 +0300

    Add files via upload

---
+++ NUnitByteInsertTest/NUnitByteInsert.cs
@@ -0,0 +1,21 @@
+﻿namespace NUnitByteInsertTest
+{
+    using System;
+
+    using NUnit.Framework;
+
+    using ByteInsertLogic;
+
+    [TestFixture]
+    public class FilterDigitTest
+    {
+        [TestCase(8, 15, 0, 0, ExpectedResult = 9)]
+        [TestCase(8, 15, 3, 8, ExpectedResult = 120)]
+        [TestCase(15, 15, 0, 0, ExpectedResult = 15)]
+        public int ByteInsertTestCase(int a, int b, int i, int j)
+        {
+            return ByteInsert.InsertNumber(a, b, i, j);
+        }
+
+    }
+}
